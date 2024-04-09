731d1ac45c387a84191206ab829d67818a70379a
RaulBirlem <raul.birlem@gmail.com>
2024-04-09 16:36:09 -0300

    creating validation with fastify-type-provider-zod

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 // ORM  Prisma
 // fastify - URL microframework
 import fastify from "fastify";
+import {serializerCompiler, validatorCompiler, ZodTypeProvider} from 'fastify-type-provider-zod'
 import {z} from 'zod';
 //criar evento:
 import { PrismaClient } from "@prisma/client";
@@ -16,29 +17,45 @@ import {generateSlug} from "../utills/generate-slug"
 
 const app = fastify()
 
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
 const prisma = new PrismaClient({
     log:['query'], // a cada query retorna um log
 })
 
-app.post('/events', async (request, reply) =>{
+    app.withTypeProvider<ZodTypeProvider>()
+    .post('/events', {
+        schema:{
+//dessa forma o prórpio fastify faz a validação dos dados
+            body:z.object({
+                title: z.string().min(4),
+                details: z.string().nullable(),
+                maximumAttendees: z.number().int().positive().nullable(),
+            }),
+            //podemos validar a resposta :
+            response: {
+                201: z.object({
+                    eventId: z.string().uuid(),
+                })
+            },
+        }
+    }, async (request, reply) =>{
 
-    const createEventSchema = z.object({
-        title: z.string().min(4),
-        details: z.string().nullable(),
-        maximumAttendees: z.number().int().positive().nullable(),
-    })
-
+    
+// validação no body com zod
     const {
         title,
         details,
         maximumAttendees
-    } = createEventSchema.parse(request.body)
+    } = request.body
     //verifica se o conteúdo em body segue a estrutura de
     //createEventSchema com o title, details e maximumAttendees
 
 
     const slug = generateSlug(title)
 
+    
 
     const eventWithSameSlug = await prisma.event.findUnique({
         where:{
@@ -46,6 +63,9 @@ app.post('/events', async (request, reply) =>{
         }
     })
 
+    if(eventWithSameSlug !== null){
+        throw new Error("Já existe evento com esse título.")
+    }
 
     const event = await prisma.event.create({
         data: {
@@ -60,7 +80,6 @@ app.post('/events', async (request, reply) =>{
     return reply.status(201).send({eventId: event.id})
 })
 
-// validação com zod
 
 
 
