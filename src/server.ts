8e22b775202528c3430ae5fe05b12063b18b69a2
RaulBirlem <raul.birlem@gmail.com>
2024-04-09 14:54:28 -0300

    app listen port

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,28 @@
-console.log("first")
+
+// fastify - URL microframework
+import fastify from "fastify";
+
+
+
+const app = fastify()
+//alguma coisa que pode demorar é uma promise
+//then ativa quando a função carregar
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen({port: 3333}).then(()=>{
+    console.log("HTTP server running!")
+})
