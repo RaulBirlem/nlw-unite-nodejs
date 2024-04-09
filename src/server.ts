1d852aa455d45f92f7fa264d08f30e9ca267dc6f
RaulBirlem <raul.birlem@gmail.com>
2024-04-09 15:04:40 -0300

    comments

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,25 @@
 // fastify - URL microframework
 import fastify from "fastify";
 
+// REST -api retorna dados via JSON
+//Métodos HTTP ...
+//Corpo da requisição (request body, post/put)
+//Parâmetros de busca (search params/ query params, get)
+//Parâmetros de rota (route params, get,put,delete)
+//Cabeçalhos (headers, informmações 'fixas' do backend) 
 
 
 const app = fastify()
-//alguma coisa que pode demorar é uma promise
-//then ativa quando a função carregar
 
 
+app.get('/',()=>{
+    return "Hello NLW Unite"
+})
 
 
+app.get('/users',()=>{
+    return "Hello NLW Unite Teste"
+})
 
 
 
@@ -26,3 +36,5 @@ const app = fastify()
 app.listen({port: 3333}).then(()=>{
     console.log("HTTP server running!")
 })
+//alguma coisa que pode demorar é uma promise
+//then ativa quando a função carregar
